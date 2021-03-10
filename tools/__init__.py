ee80cdfce9e2ee21b64602d42c53ce1c5917727c
RahulShagri <62894667+RahulShagri@users.noreply.github.com>
2021-03-11 00:23:44 +0530

    Version 0.1 initial files

--- tools/__init__.py
+++
@@ -1,6 +0,0 @@
-#-----Complete package with all tools used by SimpleDrawing-----#
-
-from tools.resetPad import resetPad
-from tools.straightLineTool import straightLineTool
-from tools.polylineTool import polylineTool
-from tools.doodleTool import doodleTool
--- tools/resetPad.py
+++
@@ -1,4 +0,0 @@
-from dearpygui.core import *
-
-def resetPad(pad_name):
-    clear_drawing(pad_name)
