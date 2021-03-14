97f9af852c755c056a61905b467ba346f0840bb5
RahulShagri <62894667+RahulShagri@users.noreply.github.com>
2021-03-14 16:01:16 +0530

    fixed bezier curve interfering with polyline

--- tools/bezierTool.py
+++ tools/bezierTool.py
@@ -28,7 +28,7 @@ def bezierTool(pad_name, lineColor, lineThickness):
             while True:
                 # Draw line
                 point2 = get_drawing_mouse_pos()
-                draw_line(pad_name, p1=mouse_position, p2=point2, color=lineColor, thickness=1, tag=f"polyLine {temp_polyline_count}")
+                draw_line(pad_name, p1=mouse_position, p2=point2, color=lineColor, thickness=1, tag=f"temp_polyline {temp_polyline_count}")
 
                 time.sleep(0.01)
 
@@ -37,7 +37,7 @@ def bezierTool(pad_name, lineColor, lineThickness):
                     # If the user clicks outside the drawing pad, it is assumed that they want to terminate the tool
                     if get_active_window() != "Drawing Pad":
                         for count in range(temp_polyline_count+1):
-                            delete_draw_command(pad_name, f"polyLine {count}")
+                            delete_draw_command(pad_name, f"temp_polyline {count}")
 
                         print("\nBezier tool terminated.")
                         return
@@ -50,7 +50,7 @@ def bezierTool(pad_name, lineColor, lineThickness):
                         while True:
                             point4 = get_drawing_mouse_pos()
 
-                            draw_line(pad_name, p1=bezier_points[2], p2=get_drawing_mouse_pos(), color=lineColor, thickness=1, tag=f"polyline {temp_polyline_count}")
+                            draw_line(pad_name, p1=bezier_points[2], p2=get_drawing_mouse_pos(), color=lineColor, thickness=1, tag=f"temp_polyline {temp_polyline_count}")
                             draw_bezier_curve(pad_name, p1=bezier_points[0], p2=bezier_points[1], p3=bezier_points[2],
                                               p4=point4, color=lineColor, thickness=lineThickness,
                                               tag=f"bezier {bezier_count}")
@@ -59,9 +59,9 @@ def bezierTool(pad_name, lineColor, lineThickness):
 
                             if is_mouse_button_released(mvMouseButton_Left):
                                 # Deleting reference poly-lines
-                                delete_draw_command(pad_name, f"polyline {temp_polyline_count}")
+                                delete_draw_command(pad_name, f"temp_polyline {temp_polyline_count}")
                                 for count in range(temp_polyline_count):
-                                    delete_draw_command(pad_name, f"polyLine {count}")
+                                    delete_draw_command(pad_name, f"temp_polyline {count}")
 
                                 if get_active_window() != "Drawing Pad":
                                     delete_draw_command(pad_name, f"bezier {bezier_count}")
@@ -75,9 +75,9 @@ def bezierTool(pad_name, lineColor, lineThickness):
                             # Check if user wants to exit the line tool
                             if is_mouse_button_released(mvMouseButton_Right):
                                 # Deleting reference poly-lines
-                                delete_draw_command(pad_name, f"polyline {temp_polyline_count}")
+                                delete_draw_command(pad_name, f"temp_polyline {temp_polyline_count}")
                                 for count in range(temp_polyline_count):
-                                    delete_draw_command(pad_name, f"polyLine {count}")
+                                    delete_draw_command(pad_name, f"temp_polyline {count}")
 
                                 delete_draw_command(pad_name, f"bezier {bezier_count}")
                                 return
@@ -85,15 +85,15 @@ def bezierTool(pad_name, lineColor, lineThickness):
                             # Check if user wants to exit the line tool
                             if is_key_released(mvKey_Escape):
                                 # Deleting reference poly-lines
-                                delete_draw_command(pad_name, f"polyline {temp_polyline_count}")
+                                delete_draw_command(pad_name, f"temp_polyline {temp_polyline_count}")
                                 for count in range(temp_polyline_count):
-                                    delete_draw_command(pad_name, f"polyLine {count}")
+                                    delete_draw_command(pad_name, f"temp_polyline {count}")
 
                                 delete_draw_command(pad_name, f"bezier {bezier_count}")
                                 return
 
                             # Delete the line drawn and begin the process again till user clicks the second point or exits the tool
-                            delete_draw_command(pad_name, f"polyline {temp_polyline_count}")
+                            delete_draw_command(pad_name, f"temp_polyline {temp_polyline_count}")
                             delete_draw_command(pad_name, f"bezier {bezier_count}")
 
                     time.sleep(0.01)
@@ -102,7 +102,7 @@ def bezierTool(pad_name, lineColor, lineThickness):
                 if is_mouse_button_released(mvMouseButton_Right):
                     # Deleting reference poly-lines
                     for count in range(temp_polyline_count):
-                        delete_draw_command(pad_name, f"polyLine {count}")
+                        delete_draw_command(pad_name, f"temp_polyline {count}")
 
                     print("\nBezier tool terminated.")
                     return
@@ -111,10 +111,10 @@ def bezierTool(pad_name, lineColor, lineThickness):
                 if is_key_released(mvKey_Escape):
                     # Deleting reference poly-lines
                     for count in range(temp_polyline_count):
-                        delete_draw_command(pad_name, f"polyLine {count}")
+                        delete_draw_command(pad_name, f"temp_polyline {count}")
 
                     print("\nBezier tool terminated.")
                     return
 
                 # Delete the line drawn and begin the process again till user clicks the second point or exits the tool
-                delete_draw_command(pad_name, f"polyLine {temp_polyline_count}")
+                delete_draw_command(pad_name, f"temp_polyline {temp_polyline_count}")
