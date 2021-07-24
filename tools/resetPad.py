f96af5f3b3f81d78c1917bca9530fd9a3fa56e3c
RahulShagri <62894667+RahulShagri@users.noreply.github.com>
2021-07-24 11:29:55 +0530

    Delete old files from cloned branch

--- tools/resetPad.py
+++
@@ -1,8 +0,0 @@
-from dearpygui.core import *
-from db_manage import reset_db
-
-def resetPad(pad_name):
-    # Erase all lines from the drawing pad
-    clear_drawing(pad_name)
-    set_item_color("Drawing Pad", style=mvGuiCol_WindowBg, color=[255, 255, 255])
-    reset_db()
